a28e05e5976e986c4043a6d90c25a76b6e379188
StarkPopStar <48656681+StarkPopStar@users.noreply.github.com>
2019-04-29 19:39:34 +0500

    task 1.3

---
+++ КМ-3/КМ-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КМ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Заданное слово: ");
+            string word = Console.ReadLine();
+            HashSet<char> characters = new HashSet<char>();
+            foreach(char el in word)
+            {
+                characters.Add(el);
+            }
+            foreach( char el in characters)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
